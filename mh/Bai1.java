10d8fae903999dbb2e25f520d71ec393f31effad
NguyenKhanhDuy101105 <nguyenkhanhduy2023dh601009@gmail.com>
2024-07-14 19:58:19 +0700

    BTVN Bai 1 & 2

---
+++ mh/Bai1.java
@@ -0,0 +1,19 @@
+package mh;
+
+import java.util.Scanner;
+
+public class Bai1 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+		for( int i=1; i<5; i++)
+			System.out.print(i+" ");
+		System.out.println();
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Nhap so n: ");
+         int n = sc.nextInt();
+         System.out.println("So vua nhap la: "+n);
+         
+	}
+
+}
---
+++ mh/Bai2.java
@@ -0,0 +1,13 @@
+package mh;
+
+import java.util.Scanner;
+
+public class Bai2 {
+   public static void main(String args[]) {
+	   System.out.println();
+	   Scanner sc = new Scanner(System.in);
+	   System.out.println("Nhap so n: ");
+	   int n = sc.nextInt();
+	   System.out.println("So n la: "+n);
+   }
+}
